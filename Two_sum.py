eb744dbe2e268af0e9ce60671299bf8fe307dc71
Poonam-CBS <82645216+Poonam-CBS@users.noreply.github.com>
2024-09-15 22:35:55 +0200

    Update Two_sum.py
    This is a code where from a given list of numbers we want to select the indices of two numbers whose sum is a given target number. Now there are several ways to do it, here we present one simple method which uses two important concepts dictionary and list. We will see the syntax to use these .

--- Two_sum.py
+++ Two_sum.py
@@ -1,10 +1,13 @@
 #This is a code to find sum of two numbers from a list of numners whose sum is equal to a target number
-#Here we write a function with two pa
+#Here we write a function with two parameters nums and target.
 def two_sum_all_pairs(nums, target):
+    #we define and initialize a dictionary lookup to store desired element from the list num as well as its index
+    #we also define and initialize a list called result where we store the result
     lookup = {}
     result = []
-
+    #for each of the index as well as element in the list num we run this loop
     for i, num in enumerate(nums):
+        # we calculate the difference between the taget and each element of nums abd store it in compliment 
         complement = target - num
         
         if complement in lookup:
