24a7fd305100d69e058089a0455ad1f489ba7635
Poonam-CBS <82645216+Poonam-CBS@users.noreply.github.com>
2024-09-16 11:20:59 +0200

    Update Leap_year.py

--- Leap_year.py
+++ Leap_year.py
@@ -1,6 +1,6 @@
-"" This is a code to determine if a given yesr is a leap year or not. Although the code is simple and require simple if statement.
+""" This is a code to determine if a given yesr is a leap year or not. Although the code is simple and require simple if statement.
 we will see how to use a function in a code and what are the elements of a fucntion.
-First we will go over the logic""
+First we will go over the logic"""
 def is_leap_year(year):
     # A year is a leap year if:
     # 1. It is divisible by 4, but not divisible by 100, or
