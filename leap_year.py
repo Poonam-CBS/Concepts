73e735815d382739f35c4008e7223e89a00dc886
Poonam-CBS <82645216+Poonam-CBS@users.noreply.github.com>
2024-09-20 17:15:48 +0200

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,10 @@
-def leap_year(inp_year):
-    if (inp_year < 0):
+#May the Force be with you!
+"""This is a code to determine if a given year is a leap year or not.
+Now to determine is if a given year is a leap year or not there are two criterias
+1. The year should be divisible by 4 but not 100
+2. The year should be divisible by 400"""
+def leap_year(inp_year): #We start a function with the parameter int_year, this will be user input later
+    if (inp_year < 0): # we check if the input is less than 0 (-ve) then we raise an error
         print("Please enter a positive value, year can not be negative")
     else:
         if (inp_year % 4 == 0):
